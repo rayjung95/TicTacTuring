9b925aa458605fe2fbbaf0ea955d09992c760499
Sanghoon Ray Jung <Ray_Jung@d192-075-241-147.wireless.sfu.ca>
2017-12-20 13:58:30 -0800

    bla

--- src/config/endpoint.js
+++
@@ -1,1 +0,0 @@
-export const relayApi = 'https://api.graph.cool/relay/v1/cjbej2b490jd10108tc7pgl0c'
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react'
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
-                <h2>Home!</h2>
-            </div>
-        )
-    }
+class Home extends Component {
 
+  render() {
+    return (
+      <div>
+        <h2>Home!</h2>
+      </div>
+    )
+  }
 }
 
 export default Home
