545b21557d8f09af368096c6c5e3568b75ebe017
Sanghoon Ray Jung <Ray_Jung@d192-075-241-147.wireless.sfu.ca>
2017-12-19 21:08:13 -0800

    our first heroku commit

---
+++ src/containers/Template.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import RaisedButton from 'material-ui/RaisedButton'
+
+
+
+injectTapEventPlugin()
+
+class Template extends Component{
+    render(){
+        return(
+            <MuiThemeProvider>
+                <div>
+                    <header>
+                        <h1>TicTacTuring</h1>
+                        <RaisedButton
+                            label={'Test Buttons'}
+                            primary={true}
+                            onTouchTap={()=>{console.log("hello, i work")}}
+                        />
+                    </header>
+                    <main>
+                        {this.props.children}
+                    </main>
+                </div>
+            </MuiThemeProvider>
+            
+        )
+    }
+
+}
+
+export default Template
